77d60b8cebd4c826424d7e9d9ffa1043b4fc5307
SonJava10Rikkei <sonjapan7@gmail.com>
2023-01-04 23:13:03 +0700

    a

---
+++ Thuc_hanh/spend-elon-fortune/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import ListItems from './components/ListItem';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <ListItems></ListItems>
+    </div>
+  );
+}
+
+export default App;
