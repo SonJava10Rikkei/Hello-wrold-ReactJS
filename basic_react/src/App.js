34de24a41f1c0d81773824e00fef48cbb3fc74a7
SonJava10Rikkei <sonjapan7@gmail.com>
2023-01-05 13:43:01 +0700

    a

--- basic_react/src/App.js
+++ basic_react/src/App.js
@@ -32,6 +32,9 @@ class App extends Component {
           address: "TP HCM"
         },
       ],
+      searchData: "",
+      sortDir: "",
+      sortBy: "",
     }
   }
   handleToggle = (status) => {
@@ -41,14 +44,58 @@ class App extends Component {
     //bật Form
     this.setState({
       isToggle: status,
+
     })
   }
 
   handleSearch = (searchData) => {
-    // Nhận dữ liệu từ searchData từ Control và thực hiện search
-    
+    // Nhận dữ liệu từ searchData từ Control và thực hiện search Rồi lưu vào state APP
+    this.setState({
+      searchData: searchData,   // searchData trái là dữ liệu của App component searchData phải là tham số trên của handleSearch
+
+    })
   }
+  handleSort = (sortDir, sortBy) => {
+    // lưu vào trong state APP
+    this.setState({
+      sortDir: sortDir,
+      sortBy: sortBy,
+    })
+  }
+
   render() {
+    // Thực hiện search (thay đổi dữ liệu của students)
+    let students = [];
+    if (this.state.searchData != "") {
+      this.state.students.forEach(st => {
+        if (st.studentName.toLowerCase().includes(this.state.searchData.toLowerCase())) {
+          students.push(st)
+        }
+      })
+    } else {
+      students = [...this.state.students];
+    }
+    // Thực hiện sort trên mảng student
+    if (this.state.sortDir != "") {
+      if (this.state.sortDir == "studentName") {
+        if (this.state.sortBy == "ASC") {
+          students.sort((a, b) => (a.studentName > b.studentName) ? 1 : (a.studentName < b.studentName) ? -1 : 0)
+        } else {
+          // sortBy = "DESC"
+          students.sort((a, b) => (a.studentName > b.studentName) ? -1 : (a.studentName < b.studentName) ? 1 : 0)
+        }
+      } else {
+        // sortDir = age
+        if (this.state.sortBy == "ASC") {
+          // students.sort((a, b) => (a.age > b.age) ? 1 : (a.age < b.age) ? -1 : 0)
+          students.sort((a, b) => (a.age - b.age))
+        } else {
+          // sortBy = "DESC"
+          // students.sort((a, b) => (a.age > b.age) ? -1 : (a.age < b.age) ? 1 : 0)
+          students.sort((a, b) => (b.age - a.age))
+        }
+      }
+    }
     let elementForm;
     if (this.state.isToggle) {
       console.log(this.state.isToggle);
@@ -62,10 +109,10 @@ class App extends Component {
           <div className="card">
             {/* START CONTROL */}
             {/*B1. Truyền props có tên là propsToggle = handleToggle */}
-            <Control propToggle={this.handleToggle} handleSearchProp={this.handleSearch}></Control>
+            <Control propToggle={this.handleToggle} handleSearchProp={this.handleSearch} handleSortProp={this.handleSort}></Control>
             {/* END CONTROL */}
             {/* START LIST STUDENT */}
-            <ListStudent propToggle={this.handleToggle} students={this.state.students}></ListStudent>
+            <ListStudent propToggle={this.handleToggle} students={students}></ListStudent>
             {/* END LIST STUDENT */}
           </div>
         </div>
--- basic_react/src/components/Control.js
+++ basic_react/src/components/Control.js
@@ -24,6 +24,7 @@ export default class Control extends Component {
         this.setState({
             searchData: value,
 
+
         });
     }
 
@@ -31,6 +32,13 @@ export default class Control extends Component {
         // thực hiện search, chuyển dữ liệu searchData sang component App
         this.props.handleSearchProp(this.state.searchData)
         event.preventDefault();
+    }
+    handleSort = (event) => {
+        // func onchange ở ô form select option
+        let value = event.target.value;
+        let arrSort = value.split("-");
+        // Truyền dữ liệu lên App
+        this.props.handleSortProp(arrSort[0], arrSort[1]);
     }
     render() {
         return (
@@ -46,7 +54,7 @@ export default class Control extends Component {
                         <div className="col-6">
                             <form className="search-form" action="#">
                                 <i className="icon-search" />
-                                <input
+                                <input style={{ height: "45px" }}
                                     type="search"
                                     className="form-control"
                                     placeholder="Search Here"
@@ -59,11 +67,12 @@ export default class Control extends Component {
                             </form>
                         </div>
                         <div className="col-3 d-flex align-items-center">
-                            <select className="form-control">
-                                <option value="">Tên tăng dần</option>
-                                <option value="">ABC def</option>
-                                <option value="">ABC def</option>
-                                <option value="">ABC def</option>
+                            <select style={{ height: "45px" }} className="form-select" onChange={this.handleSort}>
+                                <option value="">Sắp xếp theo</option>
+                                <option value="studentName-ASC">Tên tăng dần</option>
+                                <option value="studentName-DESC">Tên giảm dần</option>
+                                <option value="age-ASC">Tuổi tăng dần</option>
+                                <option value="age-DESC">Tuổi giảm dần</option>
                             </select>
                         </div>
                     </div>
