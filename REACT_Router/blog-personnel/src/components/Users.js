af8cecea56efaf5d397eddc6591b4b2771badbe4
SonJava10Rikkei <sonjapan7@gmail.com>
2023-01-12 23:35:35 +0700

    a

--- REACT_Router/blog-personnel/src/components/Users.js
+++ REACT_Router/blog-personnel/src/components/Users.js
@@ -1,7 +1,58 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import '../assets/css/Users.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Users() {
     return (
-        <div>Users</div>
+        <div className='listproducts'>
+            <div className="d-flex justify-content-around">
+               
+                <Card  style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://js0fpsb45jobj.vcdn.cloud/storage/upload/media/gumac/QC12048/1-TRANG-QC12048.png" />
+                    <Card.Body>
+                        <Card.Title>Quần ống đứng lai xéo</Card.Title>
+                        <Card.Text>
+                            477.000 đ
+                        </Card.Text>
+                        <Button className='productInfo' variant="primary">Chi tiết sản phẩm</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://js0fpsb45jobj.vcdn.cloud/storage/upload/media/gumac/ATC12047/1-DEN-ATC12047.png" />
+                    <Card.Body>
+                        <Card.Title>Áo 2 dây countdown</Card.Title>
+                        <Card.Text>
+                            184.000 đ
+                        </Card.Text>
+                        <Button className='productInfo' variant="primary">Chi tiết sản phẩm</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://js0fpsb45jobj.vcdn.cloud/storage/upload/media/gumac/VC12043/1-NAU-VC12043.png" />
+                    <Card.Body>
+                        <Card.Title>Váy A xòe lưng kiểu</Card.Title>
+                        <Card.Text>
+                            355.500 đ
+                        </Card.Text>
+                        <Button className='productInfo' variant="primary">Chi tiết sản phẩm</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://js0fpsb45jobj.vcdn.cloud/storage/upload/media/gumac/QC12080/1-DO-QC12080.png" />
+                    <Card.Body>
+                        <Card.Title>Quần ống rộng chiết ly</Card.Title>
+                        <Card.Text>
+                            495.000 đ
+                        </Card.Text>
+                        <Button className='productInfo' variant="primary">Chi tiết sản phẩm</Button>
+                    </Card.Body>
+                </Card>
+
+            </div>
+        </div>
     )
 }
--- REACT_Router/blog-personnel/src/components/layout/Header.js
+++ REACT_Router/blog-personnel/src/components/layout/Header.js
@@ -1,6 +1,10 @@
 import { React } from 'react'
 import { NavLink } from 'react-router-dom'
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
 
@@ -10,46 +14,70 @@ export default function Header() {
         backgroundColor: isActive ? 'red' : 'white',
     })
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <NavLink to={'/'} className="nav-link" style={navLinkMenuStyle}>Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={'/about'} className="nav-link" style={navLinkMenuStyle}>About</NavLink>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <NavLink to={'/users'} className="nav-link dropdown-toggle" style={navLinkMenuStyle}>Users</NavLink>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={'/contact'} className="nav-link" style={navLinkMenuStyle}>Contact</NavLink>
-                        </li>
-                    </ul>
-                    <form className="d-flex" role="search">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Ô tìm kiếm"
-                            aria-label="Search"
-                        />
-                        <button className="btn btn-outline-success" type="submit">
-                            Search
-                        </button>
-                    </form>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand >React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <><NavLink to={'/'} className="nav-link" style={navLinkMenuStyle}>Home</NavLink></>
+                        <><NavLink to={'/about'} className="nav-link" style={navLinkMenuStyle}>About</NavLink></>
+                        <NavLink to={'/users'} className="nav-link dropdown-toggle" style={navLinkMenuStyle}>Users</NavLink>
+                        <NavDropdown title="Users" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="users">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        // <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        //     <div className="container-fluid">
+        //         <a className="navbar-brand" href="#">Navbar</a>
+        //         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //             <span className="navbar-toggler-icon"></span>
+        //         </button>
+        //         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        //             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        //                 <li className="nav-item">
+        //                     <NavLink to={'/'} className="nav-link" style={navLinkMenuStyle}>Home</NavLink>
+        //                 </li>
+        //                 <li className="nav-item">
+        //                     <NavLink to={'/about'} className="nav-link" style={navLinkMenuStyle}>About</NavLink>
+        //                 </li>
+        //                 <li className="nav-item dropdown">
+        //                     <NavLink to={'/users'} className="nav-link dropdown-toggle" style={navLinkMenuStyle}>Users</NavLink>
+        //                     <ul className="dropdown-menu">
+        //                         <li><NavLink className="dropdown-item" >Action</NavLink></li>
+        //                         <li><NavLink className="dropdown-item" >Another action</NavLink></li>
+        //                         <li><NavLink className="dropdown-item" >Something else here</NavLink></li>
+        //                     </ul>
+        //                 </li>
+        //                 <li className="nav-item">
+        //                     <NavLink to={'/contact'} className="nav-link" style={navLinkMenuStyle}>Contact</NavLink>
+        //                 </li>
+        //             </ul>
+        //             <form className="d-flex" role="search">
+        //                 <input
+        //                     className="form-control me-2"
+        //                     type="search"
+        //                     placeholder="Ô tìm kiếm"
+        //                     aria-label="Search"
+        //                 />
+        //                 <button className="btn btn-outline-success" type="submit">
+        //                     Search
+        //                 </button>
+        //             </form>
 
-                </div>
-            </div>
-        </nav>
+        //         </div>
+        //     </div>
+        // </nav>
     )
 }
